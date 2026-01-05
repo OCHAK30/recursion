06c317a55b1d6f5e359a11aca1607399a04db1f5
OCHAK30 <oendrila307@gmail.com>
2026-01-05 20:23:46 +0530

    Time complexity updated

--- src/PrintOnlyOneSubsequencesWhoseSumIsK.java
+++ src/PrintOnlyOneSubsequencesWhoseSumIsK.java
@@ -1,6 +1,6 @@
 /**
  * https://www.youtube.com/watch?v=eQCS_v3bw0Q&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9&index=7
- * TC: O(2 ^ n) * n -> Every index has two possibilities O(2^n) and we are printing each subsequence as well so, O(n).
+ * TC: O(2 ^ n) -> Every index has two possibilities O(2^n) ... Note: No extra × n here because we are not printing all subsequences, just stopping at one.
  * SC: O(n) -> recursive stack space
  */
 
